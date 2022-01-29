b849b55601c0018f3da5297c11ebc7a065009220
DESKTOP-4SPP55M\Travis <godxiaoji@vip.qq.com>
2022-01-29 19:18:01 +0800

    feat(env): 加入环境判断

--- src/browser-import.js
+++ src/browser-import.js
@@ -1,24 +1,8 @@
 import Sprite from 'svg-baker-runtime/src/browser-sprite'
 import SpriteSymbol from 'svg-baker-runtime/src/browser-symbol'
-import domready from 'domready'
 
 const spriteNodeId = '__SVG_SPRITE_NODE__'
 const spriteGlobalVarName = '__SVG_SPRITE__'
-const isSpriteExists = !!window[spriteGlobalVarName]
-
-let sprite
-
-if (isSpriteExists) {
-  sprite = window[spriteGlobalVarName]
-} else {
-  sprite = new Sprite({
-    attrs: {
-      id: spriteNodeId,
-      'aria-hidden': 'true'
-    }
-  })
-  window[spriteGlobalVarName] = sprite
-}
 
 const loadSprite = () => {
   /**
@@ -35,10 +19,59 @@ const loadSprite = () => {
   }
 }
 
-if (document.body) {
-  loadSprite()
-} else {
-  domready(loadSprite)
+/*!
+ * domready (c) Dustin Diaz 2014 - License MIT
+ */
+const createDomReady = () => {
+  var fns = [],
+    listener,
+    doc = document,
+    hack = doc.documentElement.doScroll,
+    domContentLoaded = 'DOMContentLoaded',
+    loaded = (hack ? /^loaded|^c/ : /^loaded|^i|^c/).test(doc.readyState)
+
+  if (!loaded)
+    doc.addEventListener(
+      domContentLoaded,
+      (listener = function () {
+        doc.removeEventListener(domContentLoaded, listener)
+        loaded = 1
+        while ((listener = fns.shift())) listener()
+      })
+    )
+
+  return function (fn) {
+    loaded ? setTimeout(fn, 0) : fns.push(fn)
+  }
 }
+
+let sprite = new Sprite({
+  attrs: {
+    id: spriteNodeId,
+    'aria-hidden': 'true'
+  },
+  autoConfigure: typeof document !== 'undefined',
+  listenLocationChangeEvent: typeof window !== 'undefined'
+})
+
+const init = () => {
+  if (typeof window === 'undefined' || typeof document === 'undefined') {
+    return
+  }
+
+  if (window[spriteGlobalVarName]) {
+    sprite = window[spriteGlobalVarName]
+  } else {
+    window[spriteGlobalVarName] = sprite
+  }
+
+  if (document.body) {
+    loadSprite()
+  } else {
+    createDomReady()(loadSprite)
+  }
+}
+
+init()
 
 export { sprite, SpriteSymbol }
